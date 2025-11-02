e9e217fc07b241e2a268cf0e25968a7a3299e2ad
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-11-02 16:11:46 +0900

    feat: 테스트 코드 정상적인 로또 번호 출력 시, 등수 계산 메서드 테스트 기능 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -26,4 +26,31 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, 46]);
     }).toThrow("[ERROR]");
   });
+
+  // 정상적인 출력 시
+  test("정상적인 로또 번호는 예외를 발생시키지 않는다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]);
+    }).not.toThrow();
+  });
+
+  // Lotto.js의 calculateRank 메서드 기능 테스트
+  describe("등수 계산(calculateRank) 테스트", () => {
+  const winningNumbers = [1, 2, 3, 4, 5, 6];
+  const bonusNumber = 7;
+
+  // [내 로또 번호, 예상되는 등수]
+  test.each([
+    [[1, 2, 3, 4, 5, 6], 1], // 1등
+    [[1, 2, 3, 4, 5, 7], 2], // 2등
+    [[1, 2, 3, 4, 5, 8], 3], // 3등
+    [[1, 2, 3, 4, 8, 9], 4], // 4등
+    [[1, 2, 3, 8, 9, 10], 5], // 5등
+    [[10, 11, 12, 13, 14, 15], 0], // 꽝
+  ])("로또 번호 %p는 %p등입니다.", (myNumbers, expectedRank) => {
+    const lotto = new Lotto(myNumbers);
+    // RANK 상수를 임포트해서 비교
+    expect(lotto.calculateRank(winningNumbers, bonusNumber)).toBe(expectedRank);
+  });
+});
 });
--- src/constants/Error.js
+++ src/constants/Error.js
@@ -1,8 +1,8 @@
 export const ERROR = Object.freeze({
     INVALID_PURCHASE_AMOUNT: "[ERROR] 로또 금액을 숫자로 입력하고, 1000원 단위로 입력해야한다.",
     INVALID_PURCHASE_AMOUNT_COUNT: "[ERROR] 로또 구매 가능 수량은 한 회당 10개 이하만 허용됩니다.",
-    INVALID_WINNING_NUMBER: "[ERROR] 당첨 번호는 특수문자, 문자 외의 1과 45사이의 숫자를 입력해야 한다.",
+    INVALID_WINNING_NUMBER: "[ERROR] 당첨 번호는 특수문자, 문자 외의 1과 45사이의 정수를 입력해야 한다.",
     INVALID_WINNING_NUMBER_COUNT: "[ERROR] 당첨 번호는 6개의 숫자로 이루어져야 한다.",
-    INVALID_BONUS_NUMBER: "[ERROR] 보너스 번호는 특수문자, 문자 외의 1과 45사이의 숫자를 입력해야 한다.",
+    INVALID_BONUS_NUMBER: "[ERROR] 보너스 번호는 특수문자, 문자 외의 1과 45사이의 정수를 입력해야 한다.",
     BONUS_NUMBER_DUPLICATED: "[ERROR] 당첨 번호와 보너스 번호는 같으면 안된다."
 });
